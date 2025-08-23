c017e61f58552ee98f2dc32f78a0d063e76967d3
ECanDo <eric.opseth@gmail.com>
2025-08-23 00:06:49 -0600

    Conversion from coordinates to index and vice versa.
    Takes in an (int, int) and outputs an index, or (char, char).  Can also take in a (int), index, and return a string of the board position.

--- Board/board.cpp
+++ Board/board.cpp
@@ -66,3 +66,24 @@ void Board::printBoard() const {
         std::cout << "\n";
     }
 }
+
+int Board::getBoardIndex(int file, int rank) const {
+    if (file < 0 || file > 7 || rank < 0 || rank > 7)
+        return -1;
+    return file + rank * 8;
+}
+
+int Board::getBoardIndex(char file, char rank) const {
+    return getBoardIndex(file - 'a', rank - '1');
+}
+
+std::string Board::getBoardPosition(int index) const {
+    if (index < 0 || index >= 64) return "";
+
+    int rank = index >> 3; // Divide by 8
+    int file = index % 8;
+
+    return std::string()
+           + static_cast<char>(file + 'a')
+           + static_cast<char>(rank + '1');
+}
--- Board/board.h
+++ Board/board.h
@@ -34,6 +34,32 @@ public:
     // Print board
     void printBoard() const;
 
+    /**
+     * Get the square index from file and rank
+     *
+     * @param file File 0-7 (a - h)
+     * @param rank Rank 0-7 (1 - 8, with 0 = rank 1)
+     * @return Index 0..63, or -1 if out of bounds.
+     */
+    int getBoardIndex(int file, int rank) const;
+
+    /**
+     * Get the square index from file and rank
+     *
+     * @param file File 'a'-'h'
+     * @param rank Rank '1'-'8'
+     * @return Index 0..63, or -1 if out of bounds.
+     */
+    int getBoardIndex(char file, char rank) const;
+
+    /**
+     * Get the string notation of a given index. i.e. convert `0` into "a1" or `28` into "e4"
+     *
+     * @param index Index of the board to convert.
+     * @return String of the index
+     */
+    std::string getBoardPosition(int index) const;
+
 private:
     char pieceAtSquare(int square) const;
 
